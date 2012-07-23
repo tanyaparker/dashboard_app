55633333895bb2f1ff4677251476c739c7457d5f
Tanya Parker <tcparker@usc.edu>
2012-07-23 10:59:21 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DashboardApp::Application.config.secret_token = '008a50db6f7b6af561ffebb23aa0f86c2a1f6769956d6e4bd7153a20bad72680898b2167ebd001598d95e020affe3a8db1f8b76184e1320503aa91c63acd5495'
