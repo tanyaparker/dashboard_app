f0f3fe8cf941e6d79027eef1232cfdac3db35954
Tanya Parker <tcparker@usc.edu>
2012-07-27 12:41:51 +0800

    back to name

--- app/controllers/product_stats_controller.rb
+++ app/controllers/product_stats_controller.rb
@@ -2,7 +2,7 @@ class ProductStatsController < ApplicationController
   def get_features		
   	PivotalTracker::Client.token = '002c98a5157fc9cc5c180766d7c02dbd'
 	@project = PivotalTracker::Project.find(582291)
-	@project.stories.all(:state => ['started', 'finished', 'delivered', 'rejected'], :type => ['Feature', 'Bug', 'Chore'])
+	@project.stories.all(:state => ['started', 'finished', 'delivered', 'acccepted', 'rejected'], :type => ['Feature', 'Bug', 'Chore'])
 	#@story_type
 	#@current_state
 	#@name
