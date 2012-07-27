8a98f57800bd64eb050c0b48af0df249043a96b6
Tanya Parker <tcparker@usc.edu>
2012-07-27 12:39:47 +0800

    testing filters

--- app/controllers/product_stats_controller.rb
+++ app/controllers/product_stats_controller.rb
@@ -2,16 +2,9 @@ class ProductStatsController < ApplicationController
   def get_features		
   	PivotalTracker::Client.token = '002c98a5157fc9cc5c180766d7c02dbd'
 	@project = PivotalTracker::Project.find(582291)
-	@project.stories.all(:limit => '10', :offset => '20', :state => ['started', 'finished', 'delivered', 'accepted', 'rejected'], :type => 'Feature')
-	#@project.activities.all(:limit => '10')
-	@count = @project.stories.all.count
-	#@count = @project.activities.all.count
+	@project.stories.all(:state => ['started', 'finished', 'delivered', 'rejected'], :type => ['Feature', 'Bug', 'Chore'])
 	#@story_type
 	#@current_state
 	#@name
-
   end
 end
-
-#http://www.xmltoolbox.com/
-#https://github.com/jsmestad/pivotal-tracker
