b15b6d2870702dd48c77a1aff9f692aa624ccec8
Tanya Parker <tcparker@usc.edu>
2012-07-30 15:31:34 +0800

    update css, add icons, change sort by current_state

--- app/controllers/product_stats_controller.rb
+++ app/controllers/product_stats_controller.rb
@@ -1,12 +1,13 @@
 class ProductStatsController < ApplicationController
+
   def get_features		
   	PivotalTracker::Client.token = '002c98a5157fc9cc5c180766d7c02dbd'
 	@project = PivotalTracker::Project.find(582291)
 	@project.stories.all(:state => ['started', 'finished', 'delivered', 'acccepted', 'rejected'], :type => ['Feature', 'Bug', 'Chore'])
-	@count = @project.stories.all.count
-	@project.stories.all.sort! { |a,b| b.created_at <=> a.created_at } 
-	#@name, story_type, current_state
+	@project.stories.all.sort! { |a,b| b.created_at <=> a.created_at }
+	@new_project = @project.stories.all.slice(0,50) 
+	@new_project.sort! { |a,b| a.current_state <=> b.current_state}
   end 
 end
 
-
+#http://dl.dropbox.com/u/7317686/Product_RADAR/css/main.css
