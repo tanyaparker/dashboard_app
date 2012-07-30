314d33de092f27220813338ea91e1b75052d1a6d
Tanya Parker <tcparker@usc.edu>
2012-07-30 11:46:06 +0800

    sort features by creation date, story_type, and current_state

--- app/controllers/product_stats_controller.rb
+++ app/controllers/product_stats_controller.rb
@@ -3,8 +3,10 @@ class ProductStatsController < ApplicationController
   	PivotalTracker::Client.token = '002c98a5157fc9cc5c180766d7c02dbd'
 	@project = PivotalTracker::Project.find(582291)
 	@project.stories.all(:state => ['started', 'finished', 'delivered', 'acccepted', 'rejected'], :type => ['Feature', 'Bug', 'Chore'])
-	#@story_type
-	#@current_state
-	#@name
-  end
+	@count = @project.stories.all.count
+	@project.stories.all.sort! { |a,b| b.created_at <=> a.created_at } 
+	#@name, story_type, current_state
+  end 
 end
+
+
