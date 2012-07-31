72c1a84524f522fb08fb433090993cb0704bf42f
Tanya Parker <tcparker@usc.edu>
2012-07-31 10:37:56 +0800

    remove static pages from ruby tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 DashboardApp::Application.routes.draw do
 
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
-
   resources :microposts
 
   resources :users
